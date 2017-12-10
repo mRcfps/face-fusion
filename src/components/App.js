3486b9d4023f697daac33cdecd54c7c6f756da65
黄巍 <huan1043269994@gmail.com>
2017-12-10 09:59:31 +0800

    [ADD] completed index page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Upload, Icon, Modal, Button, Spin } from 'antd';
 
+// import css
+import './css/App.css';
+
+// import img
+import title from './img/title.svg';
+import content from './img/content.svg';
+import header from './img/header.svg';
+import btn from './img/btn.svg';
+
 class App extends React.Component {
 
   render() {
     return (
-      <div>hhh</div>
-    );
+      <div id="app">
+        <div className="title">
+          <img src={title} alt="title" className="titleImg"/>
+        </div>
+        <div className="content">
+          <img src={content} alt="content" className="contentImg"/>
+          <img src={header} alt="header" className="headerImg"/>
+        </div>
+        <div className="footer">
+          <img src={btn} alt="btn" className="btnImg"/>
+        </div>
+      </div>
+    );  
   }
 }
 
